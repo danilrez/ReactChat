c1c7ca62a3da1a429a6a002597fa4fe2f134d04f
CrappyCodeMaker <danil.reznichenko33@gmail.com>
2020-05-20 18:27:13 +0300

    update

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Chat from './components/Chat/Chat';
 import Join from './components/Join/Join';
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App = () => {
   return (
@@ -12,6 +12,6 @@ const App = () => {
       <Route path="/chat" component={Chat} />
     </Router>
   );
-}
+};
 
 export default App;
--- client/src/components/TextContainer/TextContainer.js
+++ client/src/components/TextContainer/TextContainer.js
@@ -6,30 +6,42 @@ import './TextContainer.css';
 
 const TextContainer = ({ users }) => (
   <div className="textContainer">
-    <div>
-      <h1>Realtime Chat Application <span role="img" aria-label="emoji">💬</span></h1>
-      <h2>Created with React, Express, Node and Socket.IO <span role="img" aria-label="emoji">❤️</span></h2>
-      <h2>Try it out right now! <span role="img" aria-label="emoji">⬅️</span></h2>
+    {users ? (
+      <div>
+        <h5>People currently chatting:</h5>
+        <div className="activeContainer">
+          <h2>
+            {users.map(({ name }) => (
+              <div key={name} className="activeItem">
+                {name}
+                <img alt="Online Icon" src={onlineIcon} />
+              </div>
+            ))}
+          </h2>
+        </div>
+      </div>
+    ) : null}
+
+    <div className="infoApp">
+      <p>
+        Realtime Chat Application{' '}
+        <span role="img" aria-label="emoji">
+          💬
+        </span>
+      </p>
+      <p>
+        Created with React, Express, Node and Socket.IO{' '}
+        <span role="img" aria-label="emoji">
+          ❤️
+        </span>
+      </p>
+      <p>
+        Try it out right now!{' '}
+        <span role="img" aria-label="emoji">
+          ⬅️
+        </span>
+      </p>
     </div>
-    {
-      users
-        ? (
-          <div>
-            <h1>People currently chatting:</h1>
-            <div className="activeContainer">
-              <h2>
-                {users.map(({name}) => (
-                  <div key={name} className="activeItem">
-                    {name}
-                    <img alt="Online Icon" src={onlineIcon}/>
-                  </div>
-                ))}
-              </h2>
-            </div>
-          </div>
-        )
-        : null
-    }
   </div>
 );
 
